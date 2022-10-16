39da4105c4afe0e5833c2d8d61ba739d60c03dc6
Ivaylo Gramatikov <IvayloGramatikov@coherentsolutions.com>
2022-10-17 00:51:11 +0300

    bug fixes

--- js/constants.js
+++
@@ -1,9 +0,0 @@
-
-var keys = ["bglanguage", "enlanguage"]
-var values = ["bg", "en"]
-const languageAbreviations = values.reduce((acc, value, i) => {
-    acc[keys[i]] = value;
-    return acc
-}, {})
-
-export { languageAbreviations }
